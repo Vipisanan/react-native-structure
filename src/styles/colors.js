73820f36e62bb8a4487331ef0b99ee81b1ef9231
vipisanan <vipisanan@huex.studio>
2021-01-20 08:13:05 +0530

    setuped structure with login

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export const PRIMARY = '#00C3B3';
+export const SECONDARY = '#4E5983';
+
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+
+// ACTIONS
+export const SUCCESS = '#3adb76';
+export const WARNING = '#ffae00';
+export const ALERT = '#cc4b37';
+
+// GRAYSCALE
+export const GRAY_LIGHT = '#FAFAFA';
+export const GRAY_MEDIUM = '#cacaca';
+export const GRAY_DARK = '#8a8a8a';
